b0633e241c63db9a14915374a57f7dfa1599325b
fahad <fass0001@student.monash.edu>
2023-10-01 21:01:32 +1100

    Req 3 mini commit - Changed quirks methods

--- src/game/actors/merchants/quirks/Quirk.java
+++ src/game/actors/merchants/quirks/Quirk.java
@@ -6,8 +6,16 @@ import game.artifacts.TransactionItem;
 public interface Quirk {
 
     // Takes actor because a vendor might have a quirk that does something to the actor
+    // (ie: actor refers to the person trading with the merchant.
     // (eg: he kicks the actor after taking his money)
-    void perform(Actor actor, TransactionItem transactionItem);
+
+    // From the point of view of the merchant
+    // (ie: the merchant is selling)
+    void performSelling(Actor actor, TransactionItem transactionItem);
+
+    // From the point of view of the merchant
+    // (ie: the merchant is purchasing)
+    void performPurchasing(Actor actor, TransactionItem transactionItem);
 
     boolean doesOccur();
 }
