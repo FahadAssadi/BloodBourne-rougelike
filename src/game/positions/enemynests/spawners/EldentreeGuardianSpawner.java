19f9004aa84bcd8d1df8f39faf66897f7683f21a
dudeishan <styxnerthegamer@gmail.com>
2023-10-29 22:45:56 +1100

    Added missing comments for javadocs generation.

--- src/game/positions/enemynests/spawners/EldentreeGuardianSpawner.java
+++ src/game/positions/enemynests/spawners/EldentreeGuardianSpawner.java
@@ -3,6 +3,11 @@ package game.positions.enemynests.spawners;
 import edu.monash.fit2099.engine.actors.Actor;
 import game.actors.enemies.EldentreeGuardian;
 
+/**
+ * A spawner for creating instances of the EldentreeGuardian enemy actor.
+ * It implements the Spawner interface and defines a spawning chance.
+ *
+ */
 public class EldentreeGuardianSpawner extends Spawner{
     private static final int DEFAULT_SPAWN_CHANCE = 20;
 
